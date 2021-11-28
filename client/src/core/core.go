f35f78c1336f99049fabd1557f40093bc3f7551d
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-28 18:24:41 +0100

    Ajout de la fonction qui permet au client de recevoir des données du serveur

--- client/src/core/core.go
+++ client/src/core/core.go
@@ -2,17 +2,32 @@
 package core
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"strings"
 )
 
-func ConnectionWithServer(ipServer string, portServer string) {
+func ConnectionWithServer(ipServer string, portServer string) net.Conn {
 	addressServer := fmt.Sprintf("%s:%s", ipServer, portServer)
 	connectionServer, err := net.Dial("tcp", addressServer)
 	if err == nil {
 		fmt.Println("[+] Connexion avec le serveur réussie ! => ", connectionServer.RemoteAddr().String())
-		connectionServer.Close()
 	} else {
 		fmt.Println("[-] Connexion avec le serveur impossible !")
 	}
+
+	return connectionServer
+}
+
+// Permet de recevoir les données du serveur
+func ReceiveDataOfServer(connectionServer net.Conn) {
+	reader := bufio.NewReader(connectionServer)
+	dataReceived, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("[-] Impossible de lire les données envoyées par le serveur !")
+	}
+	dataReceived = strings.TrimSuffix(dataReceived, "\n")
+	fmt.Println(dataReceived)
 }
--- client/src/main.go
+++ client/src/main.go
@@ -5,5 +5,7 @@ import "amolixs/core"
 
 // Fonction principal
 func main() {
-	core.ConnectionWithServer("127.0.0.1", "9090")
+	connectionServer := core.ConnectionWithServer("127.0.0.1", "9090")
+	defer connectionServer.Close()
+	core.ReceiveDataOfServer(connectionServer)
 }
