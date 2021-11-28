05431649936c90819923892c4d11fe23d30ee2c8
BlackBeard852456 <black.barbe147@gmail.com>
2021-11-28 12:28:14 +0100

    Refactorisation du project

---
+++ client/src/core/core.go
@@ -0,0 +1,8 @@
+// Paquet qui s'occupe du coeur du fonctionnement du client
+package core
+
+import "fmt"
+
+func ConnectionWithServer() {
+	fmt.Println("test")
+}
---
+++ server/src/core/core.go
@@ -0,0 +1,8 @@
+// Paquet qui s'occupe du coeur du fonctionnement du serveur
+package core
+
+import "fmt"
+
+func ConnectionWithClient() {
+	fmt.Println("To")
+}
